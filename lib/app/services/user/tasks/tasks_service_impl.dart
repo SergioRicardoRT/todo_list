21b3d3324d89c33ae52a8a1e2987e37365e77c88
Senrathy <sergioricardort@hotmail.com>
2022-10-04 07:07:11 -0300

    Services e Repository de busca de Tasks

--- lib/app/services/user/tasks/tasks_service_impl.dart
+++ lib/app/services/user/tasks/tasks_service_impl.dart
@@ -1,3 +1,5 @@
+import 'package:todo_list_provider/app/models/task_model.dart';
+import 'package:todo_list_provider/app/models/week_task_model.dart';
 import 'package:todo_list_provider/app/repositories/tasks/tasks_repository.dart';
 import 'package:todo_list_provider/app/services/user/tasks/tasks_service.dart';
 
@@ -10,4 +12,30 @@ class TasksServiceImpl extends TasksService {
   @override
   Future<void> save(DateTime date, String description) =>
       _tasksRepository.save(date, description);
+
+  @override
+  Future<List<TaskModel>> getToday() =>
+      _tasksRepository.findByPeriod(DateTime.now(), DateTime.now());
+
+  @override
+  Future<List<TaskModel>> getTomorrow() {
+    var tomorrow = DateTime.now().add(const Duration(days: 1));
+    return _tasksRepository.findByPeriod(tomorrow, tomorrow);
+  }
+
+  @override
+  Future<WeekTaskModel> getWeek() async {
+    final today = DateTime.now();
+    var startFilter = DateTime(today.year, today.month, today.day, 0, 0, 0);
+    DateTime endFilter;
+
+    if (startFilter.weekday != DateTime.monday) {
+      startFilter =
+          startFilter.subtract(Duration(days: (startFilter.weekday - 1)));
+    }
+    endFilter = startFilter.add(const Duration(days: 7));
+    final tasks = await _tasksRepository.findByPeriod(startFilter, endFilter);
+
+    return WeekTaskModel(startDate: startFilter, endDate: endFilter, tasks: tasks);
+  }
 }
