dfca9c508ccd19a92ed17798447ecc436d10c497
Senrathy <sergioricardort@hotmail.com>
2022-10-11 07:03:48 -0300

    Alterado o header para melhora

--- lib/app/modules/home/widgets/home_header.dart
+++ lib/app/modules/home/widgets/home_header.dart
@@ -8,23 +8,19 @@ class HomeHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          child: Selector<AuthProvider, String>(
-              builder: (_, value, __) {
-                return Text(
-                  'E aí, $value!',
-                  style: context.textTheme.headline5
-                      ?.copyWith(fontWeight: FontWeight.bold),
-                );
-              },
-            selector: (context, authProvider) =>
-                authProvider.user?.displayName ?? 'Não informado',
-          ),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Selector<AuthProvider, String>(
+        builder: (_, value, __) {
+          return Text(
+            'E aí, $value!',
+            style: context.textTheme.headline5
+                ?.copyWith(fontWeight: FontWeight.bold),
+          );
+        },
+        selector: (context, authProvider) =>
+            authProvider.user?.displayName ?? 'Não informado',
+      ),
     );
   }
 }
